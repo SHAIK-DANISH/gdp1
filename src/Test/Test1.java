ef2f1eed37288105270fc456b674db91677c6680
INDIA <INDIA@DESKTOP-FP492I0>
2023-01-04 22:31:42 +0530

    first commit

---
+++ src/Test/Test1.java
@@ -0,0 +1,12 @@
+package Test;
+
+public class Test1 {
+		public static void main(String[] args ) {
+			String hun="yes";
+			if (hun=="yes") 
+				System.out.println("eat something ....");
+			else
+				System.out.println("Do work");
+			
+		}
+}
